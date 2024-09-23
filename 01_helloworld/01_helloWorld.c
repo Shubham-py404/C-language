ca2da94eecd53d26011cdf444d93624cc436f4a1
Shubham Bansal <bansalshubham0105@gmail.com>
2024-09-23 22:03:16 +0530

    chapter 1 done

---
+++ 01_helloworld/01_helloWorld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+// declaring main without void or return 0 only in vs code 
+{
+  printf("hello world \n");
+  int a = 45;
+  int b = 45;
+  int c = a+b ;
+  // declaring variables like js 
+  printf("answer %d", c);
+};
