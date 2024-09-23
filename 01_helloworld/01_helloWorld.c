d633a2081c88865c3561542cf681bc629d731509
Shubham Bansal <bansalshubham0105@gmail.com>
2024-09-23 22:45:12 +0530

    done
    done

--- 01_helloworld/01_helloWorld.c
+++ 01_helloworld/01_helloWorld.c
@@ -1,13 +1,32 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 main()
-// declaring main without void or return 0 only in vs code 
+// declaring main without void or return 0 only in vs code
 {
   printf("hello world \n");
   int a = 45;
   int b = 45;
-  int c = a+b ;
-  // declaring variables like js 
-  printf("answer %d", c);
+
+  int c = a + b;
+
+  float d = 4.56567;
+
+  char e = 'a';
+  // declaring variables like js
+
+
+  printf("%d", c);
+  
+  int g ;
+  printf("enter value: ");
+  scanf("%d",&g);
+  // taking input and diplaying output
+   g=g+5;
+  printf("answer of g %d", g);
+
+
+  // \n for enter 
+  // \t for tab 
+  // escape sequence character  
 };
