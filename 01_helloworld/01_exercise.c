854cf844cf479fb4023f028d4e694b934323c706
Shubham Bansal <bansalshubham0105@gmail.com>
2024-09-25 23:10:37 +0530

    done

---
+++ 01_helloworld/01_exercise.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+// void main()
+// {
+//  int a;
+//  int b;
+//  printf("enter length \n");
+//  scanf("%d" , &a);
+//  printf("enter breadth");
+//  scanf("%d" , &b);
+//  int c = a*b;
+//  printf("answer is %d" , c );
+// }
+
+// void main()
+// {
+//     int r = 6;
+//     printf("area of circle %f", r*r*3.14);
+// }
+
+void main()
+{
+    float r ;
+    float f;
+    printf("enter temperature (c) :");
+    scanf("%f", &r);
+    f = (9.0/5.0) * r + 32;
+    printf(" converted %f", f);
+    
+}
